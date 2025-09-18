76ffed740269a2d5836f29f1897901e8757870d6
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2025-09-18 17:18:54 +0300

    home page update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -38,7 +38,7 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar/>
       {isRateLimited && <RateLimitUi/>}
-      <div className='max-w-7xl mx-auto p-4 mt-6'> 
+      <div className='max-w-7xl mx-auto p-4 mt-6 bg-black'> 
         {loading && <div className='text-center text-green-600 py-10 flex items-center justify-center'><LoaderIcon className="animate-spin size-10"/></div>}
         {notes.length === 0 && !isRateLimited && !loading && <NotesNotFound/>}
         {notes.length > 0 && !isRateLimited && (
