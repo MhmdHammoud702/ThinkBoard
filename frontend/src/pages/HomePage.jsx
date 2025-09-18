a2af9db93f1c3b79f94b5dce09915768ea8eb7a9
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2025-09-18 17:33:51 +0300

    home page update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,7 +35,7 @@ const HomePage = () => {
     fetchNotes();
   },[]);
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-black'>
       <Navbar/>
       {isRateLimited && <RateLimitUi/>}
       <div className='max-w-7xl mx-auto p-4 mt-6 bg-black'> 
